1145c5e30cfb7ddb9a6507b85677d0350848479f
Ali Naderlou <alinaderlo77@hotmail.com>
2020-07-11 23:30:19 +0430

    BusSimulation beta Finished

--- src/Components/BusSimulationMap/Map.js
+++ src/Components/BusSimulationMap/Map.js
@@ -1,71 +1,78 @@
-import React, { useState, useRef, useEffect } from 'react';
-import L from 'leaflet';
-import 'leaflet/dist/leaflet.css';
-import { Map as LeafletMap, TileLayer, Popup, Marker, FeatureGroup,Tooltip } from 'react-leaflet';
+import React, { useState, useRef, useEffect } from "react";
+import L, { marker } from "leaflet";
+import "leaflet/dist/leaflet.css";
+import {
+  Map as LeafletMap,
+  TileLayer,
+  Popup,
+  Marker,
+  FeatureGroup,
+  Tooltip,
+  Circle,
+} from "react-leaflet";
 import Control from "react-leaflet-control";
-import {FaArrowsAlt} from 'react-icons/fa';
-import './Map.scss'
+import { FaArrowsAlt } from "react-icons/fa";
+import "./Map.scss";
 delete L.Icon.Default.prototype._getIconUrl;
-
 // L.Icon.Default.mergeOptions({
 //   iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
 //   iconUrl: require("leaflet/dist/images/marker-icon.png"),
 //   shadowUrl: require("leaflet/dist/images/marker-shadow.png")
 // });
-var defaultIcon =L.icon({
+
+var defaultIcon = L.icon({
   iconUrl: require("./img/marker-icon-black.png"),
   shadowUrl: require("leaflet/dist/images/marker-shadow.png"),
-  iconSize:     [25, 41], // size of the icon
+  iconSize: [25, 41], // size of the icon
   // shadowSize:   [50, 64], // size of the shadow
-  iconAnchor:   [13, 35], // point of the icon which will correspond to marker's location
+  iconAnchor: [13, 35], // point of the icon which will correspond to marker's location
   // shadowAnchor: [4, 62],  // the same for the shadow
   // popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
 });
 var greenIcon = L.icon({
   iconUrl: require("./img/marker-icon-green.png"),
   shadowUrl: require("leaflet/dist/images/marker-shadow.png"),
-  iconSize:     [25, 41], // size of the icon
+  iconSize: [25, 41], // size of the icon
   // shadowSize:   [50, 64], // size of the shadow
-  iconAnchor:   [13, 35], // point of the icon which will correspond to marker's location
+  iconAnchor: [13, 35], // point of the icon which will correspond to marker's location
   // shadowAnchor: [4, 62],  // the same for the shadow
   // popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
 });
 var redIcon = L.icon({
   iconUrl: require("./img/marker-icon-red.png"),
   shadowUrl: require("leaflet/dist/images/marker-shadow.png"),
-  iconSize:     [25, 41], // size of the icon
+  iconSize: [25, 41], // size of the icon
   // shadowSize:   [50, 64], // size of the shadow
-  iconAnchor:   [13, 35], // point of the icon which will correspond to marker's location
+  iconAnchor: [13, 35], // point of the icon which will correspond to marker's location
   // shadowAnchor: [4, 62],  // the same for the shadow
   // popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
 });
 var blueIcon = L.icon({
   iconUrl: require("./img/marker-icon-blue.png"),
   shadowUrl: require("leaflet/dist/images/marker-shadow.png"),
-  iconSize:     [25, 41], // size of the icon
+  iconSize: [25, 41], // size of the icon
   // shadowSize:   [50, 64], // size of the shadow
-  iconAnchor:   [13, 35], // point of the icon which will correspond to marker's location
+  iconAnchor: [13, 35], // point of the icon which will correspond to marker's location
   // shadowAnchor: [4, 62],  // the same for the shadow
   // popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
 });
 export default function Map(props) {
   // const [markers, setMarkers] = useState()
-  const { markers } = props;
+  // const { markers } = props;
   const mapRef = useRef();
   const mapGroupRef = useRef();
+  const [markers,setMarkers]=useState([])
 
-  const renderMarkers = () => {
-    return markers.map(
-      (marker,index) => {
-        const markerId = marker.busCode;
-        return <Marker icon={(marker.isPinned)?blueIcon:(marker.active)?greenIcon:redIcon} onclick={() => props.onMarkerClick(markerId,index)} key={`${marker.busCode}`} position={[marker.latitude, marker.longitude]} >
-          <Tooltip className={(marker.busy)?"busy-bus-tooltip":""} permanent direction="bottom" offset={L.point(63, 1)}>{markerId}</Tooltip>
-        </Marker>
-      })
-  }
+  // const renderMarkers = () => {
+  //   return markers.map(
+  //     (marker,index) => {
+  //       const markerId = marker.busCode;
+  //       return
+  //     })
+  // }
   const fitBounds = () => {
-
-    if (mapGroupRef.current !== undefined) {  //get native Map instance
+    if (mapGroupRef.current !== undefined) {
+      //get native Map instance
       const map = mapRef.current.leafletElement;
       const group = mapGroupRef.current.leafletElement; //get native featureGroup instance
       // map.fitBounds(group.getBounds());
@@ -73,27 +80,64 @@ export default function Map(props) {
         map.fitBounds(group.getBounds());
       }
     }
-  }
+  };
   useEffect(() => {
-    fitBounds()
-  },[mapGroupRef.current]);
+    if (props.fitMarkerIntervalBounds) {
+      fitBounds();
+    }
+  }, [props.marker]);
+  useEffect(() => {
+    setMarkers(props.circles)
+  }, [props.circles]);
+  const renderCircles = (markers) => {
+    return markers.map((marker, markerIndex) => {
+      return (
+        <Circle
+          key={markerIndex}
+          center={{
+            lat: marker.latitude,
+            lng: marker.longitude,
+          }}
+          fillColor="blue"
+          radius={10}
+        />
+      );
+    });
+  };
   return (
-    <LeafletMap ref={mapRef} center={props.center} zoom={props.zoom} minZoom={7} maxZoom={19} >
+    <LeafletMap
+      ref={mapRef}
+      center={props.center}
+      zoom={props.zoom}
+      minZoom={7}
+      maxZoom={19}
+      preferCanvas={true}
+    >
       <TileLayer
-        url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       />
       <FeatureGroup ref={mapGroupRef}>
-        {(markers)?renderMarkers():null}
+        {markers.length!==0 ? (
+          <React.Fragment>
+            <Marker
+              icon={blueIcon}
+              key={`${markers[props.marker].busCode}`}
+              position={[markers[props.marker].latitude, markers[props.marker].longitude]}
+            >
+              <Tooltip permanent direction="bottom" offset={L.point(63, 1)}>
+                {markers[props.marker].busCode}
+              </Tooltip>
+            </Marker>
+            {renderCircles(markers)}
+          </React.Fragment>
+        ) : null}
       </FeatureGroup>
-      <Control position="bottomleft">
-          <button
-            className="fit-bounds-btn"
-            onClick={()=>fitBounds()}
-          >
-            <FaArrowsAlt/>
-          </button>
-        </Control>
+      <Control position="topright">
+        <button className="fit-bounds-btn" onClick={() => fitBounds()}>
+          <FaArrowsAlt />
+        </button>
+      </Control>
     </LeafletMap>
   );
 }
--- src/Pages/BusSimulation/BusSimulation.js
+++ src/Pages/BusSimulation/BusSimulation.js
@@ -1,17 +1,62 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef, useCallback } from "react";
 import Map from "../../Components/BusSimulationMap/Map";
 import "./BusSimulation.scss";
 import Ripples from "react-ripples";
 import Select from "react-select";
-import { IoMdPin } from "react-icons/io";
+import {
+  FaPlay,
+  FaStop,
+  FaPause,
+  FaMapMarkerAlt,
+  FaChevronRight,
+  FaChevronLeft,
+} from "react-icons/fa";
 import moment from "jalali-moment";
 import Loader from "../../Components/Loader/Loader";
 import { DatePicker } from "jalali-react-datepicker";
+import FakeData from "./data";
+import { marker } from "leaflet";
+// Hook
+function useEventListener(eventName, handler, element = window) {
+  // Create a ref that stores handler
+  const savedHandler = useRef();
 
+  // Update ref.current value if handler changes.
+  // This allows our effect below to always get latest handler ...
+  // ... without us needing to pass it in effect deps array ...
+  // ... and potentially cause effect to re-run every render.
+  useEffect(() => {
+    savedHandler.current = handler;
+  }, [handler]);
+
+  useEffect(
+    () => {
+      // Make sure element supports addEventListener
+      // On
+      const isSupported = element && element.addEventListener;
+      if (!isSupported) return;
+
+      // Create event listener that calls handler function stored in ref
+      const eventListener = (event) => savedHandler.current(event);
+
+      // Add event listener
+      element.addEventListener(eventName, eventListener);
+
+      // Remove event listener on cleanup
+      return () => {
+        element.removeEventListener(eventName, eventListener);
+      };
+    },
+    [eventName, element] // Re-run if eventName or element changes
+  );
+}
 const BusSimulation = () => {
   // const ws = new WebSocket('ws://193.176.241.150:8080/tms/websocket/getBusSimulation')
   const [markers, setMarkers] = useState([]);
-  const [mapCenter, setMapCenter] = useState([34.6188466, 50.8469083]);
+  const [mapCenter, setMapCenter] = useState([
+    32.654492278497646,
+    51.64067001473507,
+  ]);
   const [mapZoom, setMapZoom] = useState(12);
   const [busOptions, setBusOptions] = useState([]);
   const [selectedBusOptions, setSelectedBusOptions] = useState([]);
@@ -20,14 +65,108 @@ const BusSimulation = () => {
   const [selectedMarker, setSelectedMarker] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   //   const [busOptions,setBusOptions]=useState(false)
-  const actionMenuRef = useRef();   
+  const actionMenuRef = useRef();
   const searchBoxRef = useRef();
   const overviewBoxRef = useRef();
   const actionMenuHeaderRef = useRef();
+  const nextMarkerRef = useRef();
   const [fromDate, setFromDate] = useState(null);
   const [toDate, setToDate] = useState(null);
   const [isBusDataIsLoading, setIsBusDataIsLoading] = useState(false);
+  const [markerInterval, setMarkerInterval] = useState(0);
+  const [isTimerActive, setIsTimerActive] = useState(false);
+  const [fitMarkerIntervalBounds, setFitMarkerIntervalBounds] = useState(false);
+  const [markerIntervalSpeed, setMarkerIntervalSpeed] = useState(1000);
+  const busDetailsContainerRef = useRef();
+  const [speedOptions, setSpeedOptions] = useState([
+    {
+      id: 0,
+      speed: 1000,
+      lable: "1x",
+      selected: true,
+    },
+    {
+      id: 1,
+      speed: 500,
+      lable: "2x",
+      selected: false,
+    },
+    {
+      id: 2,
+      speed: 200,
+      lable: "4x",
+      selected: false,
+    },
+  ]);
+  function toggleTimer() {
+    setIsTimerActive(!isTimerActive);
+  }
+  function resetTimer() {
+    setMarkerInterval(0);
+    setIsTimerActive(false);
+    busDetailsContainerRef.current.scrollTo(0, 0, {
+      behavior: "smooth",
+    });
+  }
+  const handler = useCallback(
+    (e) => {
+      // Update coordinates
+      if (e.key === "ArrowRight") {
+        // console.log(nextMarkerRef)
+        // setIsTimerActive(false);
+        setMarkerInterval(markerInterval + 1);
+      }
+      if (e.key === "ArrowLeft") {
+        // console.log(nextMarkerRef)
+        // setIsTimerActive(false);
+        setMarkerInterval(markerInterval - 1);
+      }
+    },
+    [markerInterval]
+  );
+
+  // Add event listener using our hook
+  useEventListener("keyup", handler);
+  // useEffect(() => {
+  //   window.addEventListener("keydown", (e) => keyDownHandler(e));
+  //   // window.addEventListener('keyup', upHandler);
+  //   // Remove event listeners on cleanup
+  //   return () => {
+  //     window.removeEventListener('keydown',  (e) => keyDownHandler(e));
+  //     // window.removeEventListener('keyup', upHandler);
+  //   };
+  // }, [markerInterval]); // Empty array ensures that effect is only run on mount and unmount
   useEffect(() => {
+    let interval = null;
+    if (markers.length === markerInterval + 1) {
+      setIsTimerActive(false);
+      setMarkerInterval(0);
+    }
+    if (isTimerActive) {
+      interval = setInterval(() => {
+        setMarkerInterval((markerInterval) => markerInterval + 1);
+        busDetailsContainerRef.current.scrollTo(
+          0,
+          busDetailsContainerRef.current.offsetHeight *
+            parseInt(
+              markerInterval /
+                (busDetailsContainerRef.current.offsetHeight / 45)
+            ),
+          {
+            behavior: "smooth",
+          }
+        );
+      }, speedOptions.filter((item) => item.selected === true)[0].speed);
+    } else if (!isTimerActive && markerInterval !== 0) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [isTimerActive, markerInterval]);
+
+  useEffect(() => {
+    // window.addEventListener('keyup', upHandler);
+    // Remove event listeners on cleanup
+
     getBuses().then((data) => {
       console.log(data, "buses");
       var busTempOptions = data.map((item, index) => {
@@ -41,7 +180,7 @@ const BusSimulation = () => {
   }, []);
   async function getBuses() {
     let response = await fetch(
-      `http://192.168.1.155:4546/tms/api/reactService/bus/all`
+      `http://193.176.241.150:8080/tms/api/reactService/bus/all`
     );
     console.log(response);
     let data = await response.json();
@@ -49,7 +188,7 @@ const BusSimulation = () => {
   }
   async function getSelectedBusesData(inputJson) {
     let response = await fetch(
-      `http://192.168.1.155:4546/tms/api/reactService/bus/track`,
+      `http://193.176.241.150:8080/tms/api/reactService/bus/track`,
       {
         headers: { "content-type": "application/json" },
         method: "POST",
@@ -60,31 +199,106 @@ const BusSimulation = () => {
     return data;
   }
   const onSubmitBtnClick = () => {
-    setIsBusDataIsLoading(true)
+    setIsBusDataIsLoading(true);
     console.log(selectedBusOptions[0], selectedBusOptions[1]);
     const bus1 =
       selectedBusOptions[0] !== undefined ? selectedBusOptions[0].value : "0";
     const bus2 =
       selectedBusOptions[1] !== undefined ? selectedBusOptions[1].value : "0";
-   
+    // setMarkers(FakeData.busData[2]);
     getSelectedBusesData({
       busCode1: bus1,
-      busCode2: bus2,
-      tripCode: 0,
+      busCode2: 0,
+      tripCode: 9,
       fromDate: fromDate,
       toDate: toDate,
     }).then((data) => {
       console.log(data, "sdsds");
-      setMarkers(data.busData[0])
-      setIsBusDataIsLoading(false)
+      setMarkers(data.busData[9]);
+      setIsBusDataIsLoading(false);
     });
+  };
+  const handleSpeedChange = () => {
+    var selected = speedOptions.filter((item) => item.selected)[0].id;
+    // console.log();
+    var nextSelectedItem;
+    switch (selected) {
+      case 0:
+        nextSelectedItem = 1;
+        break;
+      case 1:
+        nextSelectedItem = 2;
+        break;
+      case 2:
+        nextSelectedItem = 0;
+        break;
+    }
+    setSpeedOptions(
+      speedOptions.map((item) => {
+        return {
+          ...item,
+          selected: item.id === nextSelectedItem ? true : false,
+        };
+      })
+    );
+    console.log(speedOptions)
   };
   return (
     <section className="bus-simulation-container">
       <div className="map-contianer">
-        {isLoading ? <Loader /> : <Map center={mapCenter} zoom={mapZoom} />}
+        {isLoading ? (
+          <Loader />
+        ) : (
+          <Map
+            center={mapCenter}
+            circles={markers}
+            zoom={mapZoom}
+            fitMarkerIntervalBounds={fitMarkerIntervalBounds}
+            marker={markerInterval}
+          />
+        )}
       </div>
       <div ref={actionMenuRef} className="action-menu">
+        <div className="media-controler-container">
+          <Ripples
+            onClick={() => handleSpeedChange()}
+            className="speed-control"
+          >
+            <div>
+              {speedOptions.filter((item) => item.selected === true)[0].lable}
+            </div>
+          </Ripples>
+          <Ripples
+            onClick={() => setFitMarkerIntervalBounds(!fitMarkerIntervalBounds)}
+            className={`fit-marker ${fitMarkerIntervalBounds ? "active" : ""}`}
+          >
+            <FaMapMarkerAlt />
+          </Ripples>
+          <Ripples
+            onClick={() => {
+              setIsTimerActive(false);
+              setMarkerInterval(markerInterval + 1);
+            }}
+          >
+            <FaChevronRight />
+          </Ripples>
+          <Ripples onClick={toggleTimer}>
+            {!isTimerActive ? <FaPlay /> : <FaPause />}
+          </Ripples>
+          <Ripples className="stop-btn" onClick={resetTimer}>
+            <FaStop />
+          </Ripples>
+          <Ripples
+            onClick={() => {
+              setIsTimerActive(false);
+              setMarkerInterval(markerInterval - 1);
+            }}
+          >
+            <FaChevronLeft />
+          </Ripples>
+          <div style={{ flexGrow: 1 }}></div>
+          <div className="marker-interval">{markerInterval + 1}</div>
+        </div>
         <div className="action-menu-header" ref={actionMenuHeaderRef}>
           <Select
             isLoading={busOptions.length === 0}
@@ -125,7 +339,7 @@ const BusSimulation = () => {
               <DatePicker
                 onClickSubmitButton={(momentObjFrom) => {
                   setFromDate(
-                    moment(momentObjFrom.value._d).format("YYYYMMDDhhmmss")
+                    moment(momentObjFrom.value._d).format("YYYYMMDDHHmmss")
                   );
                 }}
               />
@@ -135,7 +349,7 @@ const BusSimulation = () => {
               <DatePicker
                 onClickSubmitButton={(momentObjTo) => {
                   setToDate(
-                    moment(momentObjTo.value._d).format("YYYYMMDDhhmmss")
+                    moment(momentObjTo.value._d).format("YYYYMMDDHHmmss")
                   );
                 }}
               />
@@ -154,84 +368,31 @@ const BusSimulation = () => {
             )}
           </button>
         </div>
-        <div className="bus-detail-container">
-          {markers.map((bus) => {
-            return selectedBusOptionsString.includes(bus.busCode) ||
-              selectedBusOptionsString.includes("All") ? (
+        <div className="bus-detail-container" ref={busDetailsContainerRef}>
+          {markers.map((bus, rcdIndex) => {
+            return (
               <Ripples
-                key={bus.busCode}
-                onClick={() => {
-                  //   onBusDetailClick(bus);
-                }}
+                onClick={() => setMarkerInterval(rcdIndex)}
+                key={rcdIndex}
+                className={markerInterval === rcdIndex ? "active" : ""}
               >
-                <table
-                  className={selectedMarker === bus.busCode ? "selected" : ""}
-                >
-                  <div className="bus-code">
+                <table>
+                  {/* <div className="bus-code">
                     کد اتوبوس : {bus.busCode}
                     <div
-                      className={`pin-btn ${
-                        pinnedMarkers.includes(bus.busCode) ? "active" : ""
-                      }`}
                       //   onClick={() => onPinButtonClick(bus.busCode)}
                     >
                       <IoMdPin />
                     </div>
-                  </div>
+                  </div> */}
                   <tbody>
                     <tr>
-                      <td>
+                      <td className="index">{rcdIndex + 1}</td>
+                      <td className="speed">
                         <td>سرعت لحظه ای :</td>
                         <td className="value">{`${bus.groundSpeed}km`}</td>
                       </td>
-                      <td></td>
-                    </tr>
-                    <tr>
-                      <td>
-                        <td>شاغل/غیر شاغل:</td>
-                        <td className="value">
-                          {bus.busy ? "شاغل" : "غیر شاغل"}
-                        </td>
-                      </td>
-                      <td>
-                        <td>فعال/غیر فعال :</td>
-                        <td className="value">
-                          {bus.active ? "فعال" : "غیر فعال"}
-                        </td>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td>
-                        <td>نوع سوخت:</td>
-                        <td className="value">{bus.fuelType}</td>
-                      </td>
-                      <td>
-                        <td>وضعیت اتوبوس :</td>
-                        <td className="value">{bus.busStatus}</td>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td>
-                        <td>کد خط :</td>
-                        <td className="value">{bus.tripCode}</td>
-                      </td>
-                      <td>
-                        <td>تعداد تراکنش ها :</td>
-                        <td className="value">{bus.dcTransactionCount}</td>
-                      </td>
-                    </tr>
-                    {/* <tr>
-                      <td>
-                        <td>تراکنش های در جلو :</td>
-                        <td>{bus.frontDoorTransactionCount}</td>
-                      </td>
-                      <td className="col">
-                        <td>تراکنش های در عقب :</td>
-                        <td>{bus.backDoorTransactionCount}</td>
-                      </td>
-                    </tr> */}
-                    <tr>
-                      <td>
+                      <td className="date">
                         <td>تاریخ :</td>
                         <td
                           className="value"
@@ -242,13 +403,10 @@ const BusSimulation = () => {
                           )}
                         </td>
                       </td>
-                      <td></td>
                     </tr>
                   </tbody>
                 </table>
               </Ripples>
-            ) : (
-              ""
             );
           })}
         </div>
